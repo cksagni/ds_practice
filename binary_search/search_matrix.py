d4e693b324d7d55a39770052d71f5dc1a6b86d5b
Chandan Kumar <chandanagni91@gmail.com>
2024-07-27 21:02:45 +0530

    search an element in a 2D sorted array by flattening the array

--- binary_search/search_matrix.py
+++ binary_search/search_matrix.py
@@ -31,18 +31,43 @@ def search_matrix(matrix: List[List[int]], target: int) -> bool:
     return False
 
 
+def search_matrix_1(matrix, target):
+    left = 0
+    m = len(matrix)
+    n = len(matrix[0])
+    right = m * n - 1
+    while left <= right:
+        mid = (left + right) // 2
+        mid_row, mid_col = divmod(mid, n)
+        if matrix[mid_row][mid_col] == target:
+            return True
+        elif matrix[mid_row][mid_col] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return False
+
+
 if __name__ == "__main__":
+    nums1 = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]]
+    k = search_matrix(nums1, 13)
+    assert k is False
+    k = search_matrix_1(nums1, 13)
+    assert k is False
+
     nums1 = [[1]]
     k = search_matrix(nums1, 2)
+    assert k is False
+    k = search_matrix_1(nums1, 2)
     assert k is False
 
     nums1 = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]]
     k = search_matrix(nums1, 30)
     assert k is True
+    k = search_matrix_1(nums1, 30)
+    assert k is True
 
-    nums1 = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]]
-    k = search_matrix(nums1, 13)
-    assert k is False
+
 
 
 
